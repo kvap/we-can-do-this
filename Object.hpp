2f0abd85588340cd5a378df0a178bd2a9ce3fdcb
Constantin S. Pan <kvapen@gmail.com>
2015-01-24 15:51:41 +0500

    Implemented adding and removing tiles in editor.

--- Object.hpp
+++ Object.hpp
@@ -13,26 +13,43 @@ class Object {
 	public:
 		Object(int x, int y, int tile, sf::Texture &tilemap);
 		void draw(sf::RenderWindow &window);
+		virtual std::string getName() { return "object"; };
 		virtual void use(Object &user) = 0;
 		virtual bool usable(Object &user) { return false; };
 };
 
-class FloorJail : public Object {
-	public:
-		FloorJail(int x, int y, sf::Texture &tilemap) : Object(x, y, TILE_FLOOR_JAIL, tilemap) {};
-		void use(Object &user) {};
-};
+// A macro for avoiding lots of boilerplate code here.
+#define INERT(KLASS, ENUM, NAME) \
+	class KLASS : public Object {\
+		public:\
+			KLASS(int x, int y, sf::Texture &tilemap) : Object(x, y, ENUM, tilemap) {};\
+			std::string getName() { return NAME; };\
+			void use(Object &user) {};\
+	}
 
-class FloorLab : public Object {
-	public:
-		FloorLab(int x, int y, sf::Texture &tilemap) : Object(x, y, TILE_FLOOR_LAB, tilemap) {};
-		void use(Object &user) {};
-};
-
-class FloorQuarters : public Object {
-	public:
-		FloorQuarters(int x, int y, sf::Texture &tilemap) : Object(x, y, TILE_FLOOR_QUARTERS, tilemap) {};
-		void use(Object &user) {};
-};
+INERT(FloorJail, TILE_FLOOR_JAIL, "Jail Floor");
+INERT(FloorLab, TILE_FLOOR_LAB, "Lab Floor");
+INERT(FloorQuarters, TILE_FLOOR_QUARTERS, "Quarters Floor");
+INERT(FloorWC, TILE_FLOOR_WC, "WC Floor");
+INERT(FloorRNR, TILE_FLOOR_RNR, "RNR Floor");
+INERT(FloorCorridor, TILE_FLOOR_CORRIDOR, "Corridor Floor");
+INERT(FloorEngine, TILE_FLOOR_ENGINE, "Engine Floor");
+INERT(FloorHospital, TILE_FLOOR_HOSPITAL, "Hospital Floor");
+INERT(FloorGuard, TILE_FLOOR_GUARD, "Guard Floor");
+INERT(FloorMess, TILE_FLOOR_MESS, "Mess Floor");
+INERT(FloorBridge, TILE_FLOOR_BRIDGE, "Bridge Floor");
+INERT(Respawn, TILE_RESPAWN, "Respawn");
+INERT(Stick, TILE_STICK, "Stick");
+INERT(Toilet, TILE_TOILET, "Toilet");
+INERT(Multicooker, TILE_MULTICOOKER, "Multicooker");
+INERT(GuardStand, TILE_GUARD_STAND, "Guard Stand");
+INERT(BioChamber, TILE_BIO_CHAMBER, "Bio Chamber");
+INERT(Microscope, TILE_MICROSCOPE, "Microscope");
+INERT(Couch, TILE_COUCH, "Couch");
+INERT(Engine, TILE_ENGINE, "Engine");
+INERT(Dynamo, TILE_DYNAMO, "Dynamo");
+INERT(Hatch, TILE_HATCH, "Hatch");
+INERT(Wall, TILE_WALL, "Wall");
+INERT(Jacuzzi, TILE_JACUZZI, "Jacuzzi");
 
 #endif
--- editor.cpp
+++ editor.cpp
@@ -3,6 +3,8 @@
 
 #include "Object.hpp"
 
+const int levelSize = 128;
+
 int main() {
 	sf::ContextSettings settings;
 	settings.depthBits = 24;
@@ -17,9 +19,9 @@ int main() {
 	std::cout << "version:" << settings.majorVersion << "." << settings.minorVersion << std::endl;
 
 	const std::string texfile = "tiles.png";
-	sf::Texture texture;
-	if (!texture.loadFromFile(texfile)) {
-		std::cerr << "could not load texture: " << texfile << std::endl;
+	sf::Texture tilemap;
+	if (!tilemap.loadFromFile(texfile)) {
+		std::cerr << "could not load tilemap: " << texfile << std::endl;
 		return 1;
 	}
 
@@ -30,23 +32,25 @@ int main() {
 		return 1;
 	}
 
-	float x = 0, y = 0;
-	bool goleft = false;
-	bool goright = false;
-	bool goup = false;
-	bool godown = false;
+	int x = levelSize / 2, y = levelSize / 2;
 
-	Object *o = new FloorQuarters(0, 0, texture);
+	Object *bg[levelSize][levelSize] = {0};
+	Object *mg[levelSize][levelSize] = {0};
+	Object *fg[levelSize][levelSize] = {0};
+	Object *(*currentLayer)[128] = bg;
 
 	sf::Text text;
 	text.setFont(font);
-	text.setString(L"привет мир");
 	text.setCharacterSize(20);
 	text.setColor(sf::Color::Green);
 
+	sf::RectangleShape cursor(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+	cursor.setOutlineThickness(5);
+	cursor.setOutlineColor(sf::Color(255, 255, 127, 255));
+	cursor.setFillColor(sf::Color(255, 127, 127, 127));
+
 	sf::Clock clock;
 	while (window.isOpen()) {
-
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
@@ -54,64 +58,221 @@ int main() {
 			} else if (event.type == sf::Event::Resized) {
 				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
 				window.setView(sf::View(visibleArea));
-			} else if (event.type == sf::Event::KeyPressed) {
-				switch (event.key.code) {
-					case sf::Keyboard::Left:
-						goleft = true;
-						break;
-					case sf::Keyboard::Right:
-						goright = true;
-						break;
-					case sf::Keyboard::Up:
-						goup = true;
-						break;
-					case sf::Keyboard::Down:
-						godown = true;
-						break;
-				}
 			} else if (event.type == sf::Event::KeyReleased) {
 				switch (event.key.code) {
 					case sf::Keyboard::Left:
-						goleft = false;
+						x--;
 						break;
 					case sf::Keyboard::Right:
-						goright = false;
+						x++;
 						break;
 					case sf::Keyboard::Up:
-						goup = false;
+						y--;
 						break;
 					case sf::Keyboard::Down:
-						godown = false;
+						y++;
+						break;
+					case sf::Keyboard::Num1:
+						currentLayer = bg;
+						break;
+					case sf::Keyboard::Num2:
+						currentLayer = mg;
+						break;
+					case sf::Keyboard::Num3:
+						currentLayer = fg;
+						break;
+					case sf::Keyboard::X:
+						if (currentLayer[y][x] != NULL) {
+							delete currentLayer[y][x];
+							currentLayer[y][x] = NULL;
+						}
 						break;
 				}
+				if (currentLayer == bg) {
+					switch (event.key.code) {
+						case sf::Keyboard::W:
+							currentLayer[y][x] = new FloorWC(x, y, tilemap);
+							break;
+						case sf::Keyboard::J:
+							currentLayer[y][x] = new FloorJail(x, y, tilemap);
+							break;
+						case sf::Keyboard::L:
+							currentLayer[y][x] = new FloorLab(x, y, tilemap);
+							break;
+						case sf::Keyboard::Q:
+							currentLayer[y][x] = new FloorQuarters(x, y, tilemap);
+							break;
+						case sf::Keyboard::R:
+							currentLayer[y][x] = new FloorRNR(x, y, tilemap);
+							break;
+						case sf::Keyboard::C:
+							currentLayer[y][x] = new FloorCorridor(x, y, tilemap);
+							break;
+						case sf::Keyboard::E:
+							currentLayer[y][x] = new FloorEngine(x, y, tilemap);
+							break;
+						case sf::Keyboard::H:
+							currentLayer[y][x] = new FloorHospital(x, y, tilemap);
+							break;
+						case sf::Keyboard::G:
+							currentLayer[y][x] = new FloorGuard(x, y, tilemap);
+							break;
+						case sf::Keyboard::M:
+							currentLayer[y][x] = new FloorMess(x, y, tilemap);
+							break;
+						case sf::Keyboard::B:
+							currentLayer[y][x] = new FloorBridge(x, y, tilemap);
+							break;
+					}
+				}
+				if (currentLayer == fg) {
+					switch (event.key.code) {
+						case sf::Keyboard::P:
+							currentLayer[y][x] = new Respawn(x, y, tilemap);
+							break;
+						case sf::Keyboard::S:
+							currentLayer[y][x] = new Stick(x, y, tilemap);
+							break;
+						case sf::Keyboard::T:
+							currentLayer[y][x] = new Toilet(x, y, tilemap);
+							break;
+						case sf::Keyboard::M:
+							currentLayer[y][x] = new Multicooker(x, y, tilemap);
+							break;
+						case sf::Keyboard::G:
+							currentLayer[y][x] = new GuardStand(x, y, tilemap);
+							break;
+						case sf::Keyboard::B:
+							currentLayer[y][x] = new BioChamber(x, y, tilemap);
+							break;
+						case sf::Keyboard::Z:
+							currentLayer[y][x] = new Microscope(x, y, tilemap);
+							break;
+						case sf::Keyboard::C:
+							currentLayer[y][x] = new Couch(x, y, tilemap);
+							break;
+						case sf::Keyboard::E:
+							currentLayer[y][x] = new Engine(x, y, tilemap);
+							break;
+						case sf::Keyboard::D:
+							currentLayer[y][x] = new Dynamo(x, y, tilemap);
+							break;
+						case sf::Keyboard::H:
+							currentLayer[y][x] = new Hatch(x, y, tilemap);
+							break;
+						case sf::Keyboard::W:
+							currentLayer[y][x] = new Wall(x, y, tilemap);
+							break;
+						case sf::Keyboard::J:
+							currentLayer[y][x] = new Jacuzzi(x, y, tilemap);
+							break;
+					}
+				}
+
 			}
+		}
+		if (x < 0) {
+			x = 0;
+		}
+		if (x >= levelSize) {
+			x = levelSize - 1;
+		}
+		if (y < 0) {
+			y = 0;
+		}
+		if (y >= levelSize) {
+			y = levelSize - 1;
 		}
 
 		float dt = clock.getElapsedTime().asSeconds();
-		const float speed = 100;
-		if (goleft) {
-			x -= dt * speed;
-		}
-		if (goright) {
-			x += dt * speed;
-		}
-		if (goup) {
-			y -= dt * speed;
-		}
-		if (godown) {
-			y += dt * speed;
-		}
-
-		sf::View view = window.getView();
-		view.setCenter(sf::Vector2f(x, y));
-		window.setView(view);
-
 		clock.restart();
 
+		sf::View view = window.getView();
+		cursor.setPosition(sf::Vector2f(x * TILE_SIZE, y * TILE_SIZE));
+		view.setCenter(sf::Vector2f((x + 0.5) * TILE_SIZE, (y + 0.5) * TILE_SIZE));
+		window.setView(view);
+
 		window.clear();
-		o->draw(window);
+		for (int row = 0; row < levelSize; row++) {
+			for (int col = 0; col < levelSize; col++) {
+				if (bg[row][col] != NULL) {
+					bg[row][col]->draw(window);
+				}
+				if (currentLayer == bg) {
+					continue;
+				}
+				if (mg[row][col] != NULL) {
+					mg[row][col]->draw(window);
+				}
+				if (currentLayer == mg) {
+					continue;
+				}
+				if (fg[row][col] != NULL) {
+					fg[row][col]->draw(window);
+				}
+			}
+		}
+		window.draw(cursor);
+
+		auto winsize = window.getSize();
+		sf::View hud(sf::FloatRect(0, 0, winsize.x, winsize.y));
+		window.setView(hud);
+		std::string cellinfo = std::to_string(x) + ":" + std::to_string(y);
+
+		if (fg[y][x] == NULL) {
+			cellinfo += std::string("\nfg = NULL");
+		} else {
+			cellinfo += std::string("\nfg = " + fg[y][x]->getName());
+		}
+
+		if (mg[y][x] == NULL) {
+			cellinfo += std::string("\nmg = NULL");
+		} else {
+			cellinfo += std::string("\nmg = " + mg[y][x]->getName());
+		}
+
+		if (bg[y][x] == NULL) {
+			cellinfo += std::string("\nbg = NULL");
+		} else {
+			cellinfo += std::string("\nbg = " + bg[y][x]->getName());
+		}
+
+		if (currentLayer == bg) {
+			cellinfo += std::string("\ncurrent bg");
+			cellinfo += std::string("\nw - wc floor");
+			cellinfo += std::string("\nj - jail floor");
+			cellinfo += std::string("\nl - lab floor");
+			cellinfo += std::string("\nq - quarters floor");
+			cellinfo += std::string("\nr - rnr floor");
+			cellinfo += std::string("\nc - corridor floor");
+			cellinfo += std::string("\ne - engine floor");
+			cellinfo += std::string("\nh - hospital floor");
+			cellinfo += std::string("\ng - guard floor");
+			cellinfo += std::string("\nm - mess floor");
+			cellinfo += std::string("\nb - bridge floor");
+		} else if (currentLayer == mg) {
+			cellinfo += std::string("\ncurrent mg");
+		} else {
+			cellinfo += std::string("\ncurrent fg");
+			cellinfo += std::string("\np - respawn");
+			cellinfo += std::string("\ns - stick");
+			cellinfo += std::string("\nt - toilet");
+			cellinfo += std::string("\nm - multicooker");
+			cellinfo += std::string("\ng - guard stand");
+			cellinfo += std::string("\nb - bio chamber");
+			cellinfo += std::string("\nz - microscope");
+			cellinfo += std::string("\nc - couch");
+			cellinfo += std::string("\ne - engine");
+			cellinfo += std::string("\nd - dynamo");
+			cellinfo += std::string("\nh - hatch");
+			cellinfo += std::string("\nw - wall");
+			cellinfo += std::string("\nj - jacuzzi");
+		}
+
+		text.setString(cellinfo);
 		window.draw(text);
 
+		window.setView(view);
 		window.display();
 	}
 
